c5c3a67f0d64a9b385e4589c8864c1cb07216c75
venkatamahesh <mahesh@gmail.com>
2021-05-16 19:16:16 +0530

    firstadd

---
+++ practicesamples/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace practicesamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            operators op = new operators();
+            op.Arithmetic(10, 20);
+            op.Relational(20, 30);
+            op.Logical();
+            conditional cl = new conditional();
+            cl.statement1(23);
+            cl.statement2(1);
+            loops ls = new loops();
+            ls.loop1();
+            ls.loop2();
+            ls.loop3();
+            jump jm = new jump();
+            jm.statement1();
+            jm.statement2();
+            jm.statement3();
+
+        }
+    }
+}
---
+++ practicesamples/loops.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practicesamples
+{
+    class loops
+    {
+        public void loop1()
+        {
+            Console.WriteLine("For Loop");
+            for(int i = 1; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public void loop2()
+        {
+            Console.WriteLine("While Loop");
+            int n = 1;
+            while (n <= 4)
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+        }
+        public void loop3()
+        {
+            Console.WriteLine("DoWhile Loop");
+            int n =1;
+            do
+            {
+                Console.WriteLine(n);
+                n++;
+            } while (n<4);
+        }
+    }
+}
